7594c695c8f12b162f5d626ceced0c47d73fc83e
gespel <heimbrodt@uni-potsdam.de>
2024-07-28 20:16:52 +0200

    lol

--- src/main/java/de/heimbrodt/sten/Main.java
+++ src/main/java/de/heimbrodt/sten/Main.java
@@ -4,6 +4,7 @@ import de.heimbrodt.sten.backend.StensWateringBackend;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        //I love it simple
         StensWateringBackend swb = new StensWateringBackend("");
         swb.run();
     }
